3c0837b9fd06aa4faa51d65f52739665d04dfd56
moumita1991 <s.moumita91@gmail.com>
2016-09-02 21:07:59 +0530

    added var name

--- admin/users/user.crud.php
+++ admin/users/user.crud.php
@@ -2,6 +2,7 @@
 	include_once $_SERVER ['DOCUMENT_ROOT'].'/admin_php/config/dbconnect.php';
 
 	class CRUDUser {
+		public $userTable = 'users';
  public function __construct()
  {
   $db = new DB_con();
@@ -9,31 +10,31 @@
  
  public function create($name,$email,$password,$username)
  {
-  mysql_query("INSERT INTO users(name,email,password,username) VALUES('$name','$email','$password','$username')");
+  mysql_query("INSERT INTO ".$this->userTable."(name,email,password,username) VALUES('$name','$email','$password','$username')");
  }
 
  public function userExists($username,$password) {
- 	return mysql_query("SELECT * from users WHERE username='$username' AND password='$password'");
+ 	return mysql_query("SELECT * from ".$this->userTable." WHERE username='$username' AND password='$password'");
  }
  
  public function read()
  {
-  return mysql_query("SELECT * FROM users");
+  return mysql_query("SELECT * FROM ".$this->userTable."");
  }
 
  public function readSingle($id)
  {
-  return mysql_query("SELECT * FROM users WHERE user_id=".$id);
+  return mysql_query("SELECT * FROM ".$this->userTable." WHERE user_id=".$id);
  }
  
  public function delete($id)
  {
-  mysql_query("DELETE FROM users WHERE user_id=".$id);
+  mysql_query("DELETE FROM ".$this->userTable." WHERE user_id=".$id);
  }
  
  public function update($name,$email,$password,$username)
  {
-  mysql_query("UPDATE users SET name='$name', email='$email', password='$password',username=$username  WHERE user_id=".$id);
+  mysql_query("UPDATE ".$this->userTable." SET name='$name', email='$email', password='$password',username=$username  WHERE user_id=".$id);
  }
 }
 ?>
